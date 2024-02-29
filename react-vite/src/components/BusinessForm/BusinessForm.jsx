4da7bf5d80d16040488d130f72babe5d59e811b6
Gary Cheung <garycheung78@gmail.com>
2024-02-29 15:28:26 -0500

    working on the business form and thunk

--- react-vite/src/components/BusinessForm/BusinessForm.jsx
+++ react-vite/src/components/BusinessForm/BusinessForm.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 // import { useDispatch } from "react-redux";
 // import { useNavigate } from "react-router-dom";
+import './BusinessForm.css';
 
 function BusinessForm() {
     // const dispatch = useDispatch()
     // const navigate = useNavigate()
 
     const [businessName, setBusinessName] = useState('')
-    // const [phoneNumber, setPhoneNumber] = useState('')
-    // const [address, setAddress] = useState('')
-    // const [city, setCity] = useState('')
-    // const [state, setState] = useState('')
-    // const [zipCode, setZipCode] = useState('')
-    // const [description, setDescription] = useState('')
-    // const [latitude, setLatitude] = useState('')
-    // const [longitude, setLongitude] = useState('')
-    // const [priceRange, setPriceRange] = useState()
-    // const [businessURL, setBusinessURL] = useState('')
-    // const [businessImage, setBusinessImage] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [address, setAddress] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [zipCode, setZipCode] = useState('')
+    const [description, setDescription] = useState('')
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [priceRange, setPriceRange] = useState()
+    const [businessURL, setBusinessURL] = useState('')
+    const [businessImage, setBusinessImage] = useState('')
 
 
     return (
@@ -25,15 +26,102 @@ function BusinessForm() {
             <h1>THIS IS THE BUSINESS FORM PAGE</h1>
             <form>
                 <div>
-                    <input
-                        className='create-business-input'
-                        type='text'
-                        placeholder='Business name'
-                        value={businessName}
-                        onChange={(e) => {
-                            setBusinessName(e.target.value)
-                        }}
-                        required />
+                    <label>
+                        Name
+                        <input
+                            className='create-business-input'
+                            type='text'
+                            placeholder='Business Name'
+                            value={businessName}
+                            onChange={(e) => {
+                                setBusinessName(e.target.value)
+                            }}
+                            required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Phone Number
+                        <input
+                            className='create-business-input'
+                            type='text'
+                            placeholder='xxx-xxx-xxxx'
+                            value={phoneNumber}
+                            onChange={(e) => {
+                                setPhoneNumber(e.target.value)
+                            }}
+                            required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Address
+                        <input
+                            className='create-business-input'
+                            type='text'
+                            placeholder='Street Address'
+                            value={address}
+                            onChange={(e) => {
+                                setAddress(e.target.value)
+                            }}
+                            required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        City
+                        <input
+                            className='create-business-input'
+                            type='text'
+                            placeholder='City'
+                            value={city}
+                            onChange={(e) => {
+                                setCity(e.target.value)
+                            }}
+                            required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        State
+                        <input
+                            className='create-business-input'
+                            type='text'
+                            placeholder='State'
+                            value={state}
+                            onChange={(e) => {
+                                setState(e.target.value)
+                            }}
+                            required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        ZIP Code
+                        <input
+                            className='create-business-input'
+                            type='number'
+                            placeholder='ZIP Code'
+                            value={zipCode}
+                            onChange={(e) => {
+                                setZipCode(e.target.value)
+                            }}
+                            required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Description
+                        <input
+                            className='create-business-input'
+                            type='text  '
+                            placeholder='Please tell me something about your business...'
+                            value={description}
+                            onChange={(e) => {
+                                setDescription(e.target.value)
+                            }}
+                            required />
+                    </label>
                 </div>
             </form>
         </div>
---
+++ react-vite/src/redux/business.js
@@ -0,0 +1,92 @@
+const LOAD_ALL = 'businesses/LOAD_ALL';
+const LOAD_ONE = 'businesses/LOAD_ONE';
+const CREATE_BUSINESS = 'businesses/CREATE';
+
+const loadAll = (businesses) => ({
+    type: LOAD_ALL,
+    businesses
+});
+
+const loadOne = (business) => ({
+    type: LOAD_ONE,
+    business
+})
+
+const createBusiness = (business) => ({
+    type: CREATE_BUSINESS,
+    business
+})
+
+export const thunkGetAllBusinesses = () => async (dispatch) => {
+    const response = await fetch ('/api/businesses')
+
+    if (response.ok) {
+        const allBusinesses = await response.json();
+        dispatch(loadAll(allBusinesses));
+        return allBusinesses;
+    } else {
+        const error = await response.json();
+        return error;
+    }
+}
+
+export const thunkGetOneBusiness = (businessId) => async (dispatch) => {
+    const response = await fetch (`/api/businesses/${businessId}`)
+
+    if (response.ok) {
+        const oneBusiness = await response.json();
+        dispatch(loadOne(oneBusiness));
+        return oneBusiness;
+    } else {
+        const error = await response.json();
+        return error;
+    }
+}
+
+export const thunkCreateBusiness = (business) => async (dispatch) => {
+    const response = await fetch ('/api/businesses/new', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(business)
+    })
+    if (response.ok) {
+        const newBusiness = await response.json();
+        dispatch(createBusiness(newBusiness));
+        return newBusiness
+    } else {
+        const error = await response.json();
+        return error
+    }
+}
+
+const initialState = {
+    allBusinesses: {},
+    oneBusiness: {}
+};
+
+const businessReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_ALL:
+            return {
+                ...state,
+                allBusinesses: action.businesses
+            }
+        case LOAD_ONE:
+            return {
+                ...state,
+                oneBusiness: action.business
+            }
+        case CREATE_BUSINESS:
+            return {
+                ...state, 
+                allBusinesses: {
+                    ...state.allBusinesses,
+                    [action.business.id]: action.business
+                }
+            }
+        default:
+            return state
+    }
+}
+
+export default businessReducer;
